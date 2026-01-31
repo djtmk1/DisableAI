6e79fcb57402e8ed136d41dd6a39f9fe4c1c6f73
IM-IgniteDEV <IgniteDEVContact@gmail.com>
2026-02-01 00:41:50 +0100

    Added @Getter for aiManager and messages fields Split onEnable() into 4 methods: initializeConfig(), initializeManager(), registerListeners(), registerCommands() Added reloadAllConfigs() method for reloading config.yml, messages.yml and entities Null-safe command registration with Optional.ofNullable()

--- src/main/java/com/djtmk/disableai/DisableAI.java
+++ src/main/java/com/djtmk/disableai/DisableAI.java
@@ -1,16 +1,60 @@
 package com.djtmk.disableai;
 
-import com.djtmk.disableai.events.Events;
-import com.djtmk.disableai.manager.AIManager;
 import com.djtmk.disableai.commands.DisableAICommand;
+import com.djtmk.disableai.commands.DisableAITabCompleter;
+import com.djtmk.disableai.config.Messages;
+import com.djtmk.disableai.events.EntitySpawnListener;
+import com.djtmk.disableai.manager.AIManager;
+import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Optional;
+import java.util.logging.Level;
+
 public final class DisableAI extends JavaPlugin {
+
+    @Getter
+    private AIManager aiManager;
+
+    @Getter
+    private Messages messages;
+
+    @Override
     public void onEnable() {
         saveDefaultConfig();
-        AIManager aiManager = new AIManager(this);
-        aiManager.update();
-        getServer().getPluginManager().registerEvents(new Events(aiManager), this);
-        getCommand("disableai").setExecutor(new DisableAICommand(this, aiManager));
+        initializeConfig();
+        initializeManager();
+        registerListeners();
+        registerCommands();
+    }
+
+    private void initializeConfig() {
+        this.messages = new Messages(this);
+    }
+
+    private void initializeManager() {
+        this.aiManager = new AIManager(this);
+        this.aiManager.updateAllEntities();
+    }
+
+    private void registerListeners() {
+        getServer().getPluginManager().registerEvents(new EntitySpawnListener(aiManager), this);
+    }
+
+    private void registerCommands() {
+        Optional.ofNullable(getCommand("disableai"))
+                .ifPresentOrElse(
+                        command -> {
+                            command.setExecutor(new DisableAICommand(this, aiManager, messages));
+                            command.setTabCompleter(new DisableAITabCompleter(aiManager));
+                        },
+                        () -> getLogger().log(Level.SEVERE, "Failed to register 'disableai' command!")
+                );
+    }
+
+    public void reloadAllConfigs() {
+        reloadConfig();
+        messages.loadConfig();
+        aiManager.updateAllEntities();
     }
 }
